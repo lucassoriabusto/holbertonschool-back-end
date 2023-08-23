f793e03009726324a44a9aa485ef874815e93e7a
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-22 17:41:29 -0700

    Task 00

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" """
-
-
+""" Write a Python script that, using this REST API,
+for a given employee ID, returns information about his/her
+TODO list progress."""
 import requests
 import sys
+
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
