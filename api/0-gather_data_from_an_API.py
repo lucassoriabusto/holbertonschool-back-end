67a5b025cd736fbec1ce87b2d6eae9ab4df3f288
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-22 21:30:14 -0700

    Task 00

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,19 +9,25 @@ import sys
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    response = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     employee_data = response.json()
 
-    todo_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    todo_response = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
     todo_data = todo_response.json()
 
     total_tasks = len(todo_data)
     completed_tasks = 0
-    for task in todo_data:
-        if task['completed']:
-            completed_tasks += 1
+    completed_task_titles = []
 
-    print(f"Employee {employee_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
     for task in todo_data:
         if task["completed"]:
-            print("\t", task["title"])
+            completed_tasks += 1
+            completed_task_titles.append(task["title"])
+
+    print(f"Employee {employee_data['name']} "
+          f"is done with tasks({completed_tasks}/{total_tasks}):")
+
+    for task in completed_task_titles:
+        print(f"\t {task}")
