1d4924c77cec1e12e07f018f151776397dd06e04
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-27 15:15:13 -0700

    Task 02

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Write a Python script that, using this REST API,
+for a given employee ID, returns information about his/her
+TODO list progress."""
+
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_data = response.json()
+
+    todo_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    todo_data = todo_response.json()
+
+    USER_ID = employee_data['id']
+    USERNAME = employee_data['username']
+
+    json_filename = f"{USER_ID}.json"
+
+    """ Creates an empty list where it will store in each iteration,
+    a dictionary with the tasks and their status. """
+    tasks_list = []
+    for task in todo_data:
+        TASK_TITLE = task['title']
+        TASK_COMPLETED_STATUS = task['completed']
+        tasks_list.append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME})
+
+    """ Dictionary with the user id as key and a list as value. """
+    data_to_export = {str(USER_ID): tasks_list}
+
+    with open(json_filename, 'w') as f:
+        json.dump(data_to_export, f)
